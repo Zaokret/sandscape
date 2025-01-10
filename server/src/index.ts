a2b246324212e963356abefb2d8cd65ec03cd409
Lazar Todorovic <vulalaza@gmail.com>
2025-01-10 02:13:16 +0100

    add missing discord intents and partials

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,26 @@
 // Require the necessary discord.js classes
-import { ClientOptions, GatewayIntentBits } from "discord.js"
+import { ClientOptions, GatewayIntentBits, Partials } from "discord.js"
+import { ExtendedClient } from "./client"
 import { getCommands } from "./commands"
 import { getEvents } from "./events"
-import { ExtendedClient } from "./client"
-import { createDbSeed } from "./database/seed"
 
 async function main() {
-  await createDbSeed()
   const token = process.env.DISCORD_TOKEN!
-  const options = { intents: [GatewayIntentBits.Guilds] } satisfies ClientOptions
+
+  const options = {
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildMessageReactions,
+      GatewayIntentBits.MessageContent,
+    ],
+    partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+  } satisfies ClientOptions
+
   const commands = await getCommands()
   const events = await getEvents()
   const client = new ExtendedClient(options, commands, events)
-  client.login(token)
+  await client.login(token)
 }
 
 main()
